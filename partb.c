91ffa7643a0bc91d036417b35d1f4992e738a5c2
maldonadoj4atwit <maldonadoj4atwit>
2025-11-14 22:48:32 -0500

    Completed Part B

--- partb.c
+++ partb.c
@@ -6,6 +6,45 @@
 #include <sys/wait.h>
 
 int main(int argc, char* argv[]) {
-    // TODO: Complete and document
-    return 0;
+    
+    //checks arguments
+    if ( argc != 3 ) {
+        fprintf( stderr, "USAGE: partb FILEIN FILEOUT\n" ) ;
+        return 1 ;
+    }
+
+    char* input_filename = argv[ 1 ] ;
+    char* output_filename = argv[ 2 ] ;
+
+    //opens the output file
+    int fd = open( output_filename, O_WRONLY | O_CREAT | O_TRUNC, 0770 ) ;
+    if ( fd < 0 ) {
+        perror( "ERROR: could not open output file" ) ;
+        return 2 ;
+    }
+
+    //redirects STDOUT to the file
+    if ( dup2( fd, STDOUT_FILENO ) < 0 ) {
+        perror( "ERROR: dup2 failed" ) ;
+        close( fd ) ;
+        return 3 ;
+    }
+    // original fd no longer needed after dup2
+    close(fd); 
+
+    //prints the header and flush
+    printf( "Category,Count\n" ) ;
+    fflush( stdout ) ;
+
+    //prepares arguments for execv for parta
+    char* parta_args[] = { "./parta", input_filename, NULL } ;
+
+    //execute parta
+    int eret = execv( "./parta", parta_args ) ;
+
+    //only if execv fails
+    perror( "ERROR: execv failed" ) ;
+
+    return 4 ;
+
 }
